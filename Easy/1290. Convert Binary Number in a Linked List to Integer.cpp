c1c462830fcb30f154d5f2e2b865884824f2ec0a
Ranjith <105041389+Ranjith2806@users.noreply.github.com>
2022-12-17 23:55:56 +0530

    Convert Binary Number in a Linked List to Integer
    Link to Problem
    https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/

---
+++ Easy/1290. Convert Binary Number in a Linked List to Integer.cpp
@@ -0,0 +1,23 @@
+/*Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+Return the decimal value of the number in the linked list.
+
+The most significant bit is at the head of the linked list.
+*/
+
+
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int ans=0;
+        ListNode* temp=head;
+        while(temp!=NULL)
+        {
+            ans*=2;
+            ans+=(temp->val);
+            temp=temp->next;
+        }
+        return ans;
+
+    }
+};
